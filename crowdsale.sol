779db66607ee8427d1a2e7c6f26b426f90e13fd9
Brainaut DeFi <80780137+Brainaut@users.noreply.github.com>
2021-03-19 00:43:15 +0100

    Add files via upload

---
+++ crowdsale.sol
@@ -0,0 +1,259 @@
+
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.6.12;
+
+interface IBEP20 {
+    function totalSupply() external view returns (uint);
+    function balanceOf(address account) external view returns (uint);
+    function transfer(address recipient, uint amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint);
+    function approve(address spender, uint amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+library SafeMath {
+    function add(uint a, uint b) internal pure returns (uint) {
+        uint c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+    function sub(uint a, uint b) internal pure returns (uint) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+    function sub(uint a, uint b, string memory errorMessage) internal pure returns (uint) {
+        require(b <= a, errorMessage);
+        uint c = a - b;
+
+        return c;
+    }
+    function mul(uint a, uint b) internal pure returns (uint) {
+        if (a == 0) {
+            return 0;
+        }
+
+        uint c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+    function div(uint a, uint b) internal pure returns (uint) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+    function div(uint a, uint b, string memory errorMessage) internal pure returns (uint) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0, errorMessage);
+        uint c = a / b;
+
+        return c;
+    }
+}
+
+
+library Address {
+    function isContract(address account) internal view returns (bool) {
+        bytes32 codehash;
+        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+        // solhint-disable-next-line no-inline-assembly
+        assembly { codehash := extcodehash(account) }
+        return (codehash != 0x0 && codehash != accountHash);
+    }
+}
+library SafeERC20 {
+    using SafeMath for uint;
+    using Address for address;
+
+    function safeTransfer(IBEP20 token, address to, uint value) internal {
+        callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
+    }
+
+    function safeTransferFrom(IBEP20 token, address from, address to, uint value) internal {
+        callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+    }
+
+    function safeApprove(IBEP20 token, address spender, uint value) internal {
+        require((value == 0) || (token.allowance(address(this), spender) == 0),
+            "SafeERC20: approve from non-zero to non-zero allowance"
+        );
+        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
+    }
+    function callOptionalReturn(IBEP20 token, bytes memory data) private {
+        require(address(token).isContract(), "SafeERC20: call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = address(token).call(data);
+        require(success, "SafeERC20: low-level call failed");
+
+        if (returndata.length > 0) { // Return data is optional
+            // solhint-disable-next-line max-line-length
+            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
+        }
+    }
+}
+
+contract sale{
+    using SafeMath for uint256;
+    using SafeERC20 for IBEP20;
+
+    // The token being sold
+    IBEP20 private _token;
+
+    // Address where funds are collected
+    address payable private _wallet;
+
+    // How many token units a buyer gets per BNB
+ 
+    uint256 private _rate = 160000 ;
+
+    // Amount of jagger raised
+    uint256 private _fundsRaised;
+
+    /**
+     * Event for token purchase logging
+     * @param purchaser who paid for the tokens
+     * @param beneficiary who got the tokens
+     * @param value weis paid for purchase
+     * @param amount amount of tokens purchased
+     */
+    event TokensPurchased(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+
+    constructor (address payable wallet, IBEP20 token) public {
+      
+        require(wallet != address(0), "Crowdsale: wallet is the zero address");
+        require(address(token) != address(0), "Crowdsale: token is the zero address");
+
+       
+        _wallet = wallet;
+        _token = token;
+    }
+
+ 
+    fallback () external payable {
+        buyTokens(msg.sender);
+    }
+    
+    
+    receive () external payable {
+        buyTokens(msg.sender);
+    }
+
+    /**
+     * @return the token being sold.
+     */
+    function token() public view returns (IBEP20) {
+        return _token;
+    }
+
+    /**
+     * @return the address where funds are collected.
+     */
+    function wallet() public view returns (address payable) {
+        return _wallet;
+    }
+
+    /**
+     * @return the number of token units a buyer gets per jagger.
+     */
+    function rate() public view returns (uint256) {
+        return _rate;
+    }
+
+    /**
+     * @return the amount of funds raised.
+     */
+    function fundsRaised() public view returns (uint256) {
+        return _fundsRaised;
+    }
+
+  
+    function buyTokens(address beneficiary) public payable {
+        uint256 Amount = msg.value;
+        require(Amount >= 1 ether || Amount <= 20 ether, "insufficient BNB");
+      
+        _preValidatePurchase(beneficiary, Amount);
+
+        // calculate token amount to be created
+        uint256 tokens = _getTokenAmount(Amount);
+
+        // update state
+        _fundsRaised = _fundsRaised.add(Amount);
+
+        _processPurchase(beneficiary, tokens);
+        emit TokensPurchased(msg.sender, beneficiary, Amount, tokens);
+
+        _updatePurchasingState(beneficiary, Amount);
+
+        _forwardFunds();
+        _postValidatePurchase(beneficiary, Amount);
+    }
+
+    /**
+
+     * @param beneficiary Address performing the token purchase
+     * @param Amount Value in wei involved in the purchase
+     */
+    function _preValidatePurchase(address beneficiary, uint256 Amount) internal view {
+        require(beneficiary != address(0), "Crowdsale: beneficiary is the zero address");
+        require(Amount != 0, "Crowdsale: Amount is 0");
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+    }
+
+    /**
+     * @dev Validation of an executed purchase. Observe state and use revert statements to undo rollback when valid
+     * conditions are not met.
+     * @param beneficiary Address performing the token purchase
+     * @param Amount Value in wei involved in the purchase
+     */
+    function _postValidatePurchase(address beneficiary, uint256 Amount) internal view {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+
+    /**
+     * @dev Source of tokens. Override this method to modify the way in which the crowdsale ultimately gets and sends
+     * its tokens.
+     * @param beneficiary Address performing the token purchase
+     * @param tokenAmount Number of tokens to be emitted
+     */
+    function _deliverTokens(address beneficiary, uint256 tokenAmount) internal {
+        _token.transfer(beneficiary, tokenAmount);
+    }
+
+    /**
+     * @dev Executed when a purchase has been validated and is ready to be executed. Doesn't necessarily emit/send
+     * tokens.
+     * @param beneficiary Address receiving the tokens
+     * @param tokenAmount Number of tokens to be purchased
+     */
+    function _processPurchase(address beneficiary, uint256 tokenAmount) internal {
+        _deliverTokens(beneficiary, tokenAmount);
+    }
+
+    /**
+     * @dev Override for extensions that require an internal state to check for validity (current user contributions,
+     * etc.)
+     * @param beneficiary Address receiving the tokens
+     * @param Amount Value in wei involved in the purchase
+     */
+    function _updatePurchasingState(address beneficiary, uint256 Amount) internal {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+
+    /**
+     * @dev Override to extend the way in which ether is converted to tokens.
+     * @param Amount Value in wei to be converted into tokens
+     * @return Number of tokens that can be purchased with the specified _weiAmount
+     */
+    function _getTokenAmount(uint256 Amount) public view returns (uint256) {
+        return (Amount * _rate)/ (10**11);
+    }
+
+    /**
+     * @dev Determines how ETH is stored/forwarded on purchases.
+     */
+    function _forwardFunds() internal {
+        _wallet.transfer(msg.value);
+    }
+}
